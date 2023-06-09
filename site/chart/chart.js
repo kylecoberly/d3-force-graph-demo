f8e5e161767d7403ddbf0603b1a4de2ee580c6da
Kyle Coberly <kyle.coberly@gmail.com>
2023-06-09 03:14:36 -0600

    Refactor scaling

--- site/chart/chart.js
+++ site/chart/chart.js
@@ -1,11 +1,14 @@
 import { select, zoom } from "d3"
 
+const height = 800
+const width = 800
+
 export const svg = select("#container")
 	.append("svg")
 	.attr("preserveAspectRatio", "xMinYMin meet")
-	.attr("viewBox", [-50, -50, 100, 100])
-	.attr("width", "100%")
-	.attr("height", "100%")
+	.attr("viewBox", [width / -2, height / -2, width, height])
+	.attr("width", `${width}px`)
+	.attr("height", `${height}px`)
 
 export const bounds = svg
 	.append("g")
@@ -37,14 +40,18 @@ defs
 	.append("symbol")
 	.attr("id", "circle")
 	.attr("viewBox", "-5 -5 10 10")
-	.attr("width", 5)
-	.attr("height", 5)
+	.attr("width", 20)
+	.attr("height", 20)
 	.attr("r", 4)
 	.attr("cx", 0)
 	.attr("cr", 0)
 
 // Enable Zoom
-export const zoomer = zoom().scaleExtent([1 / 2, 3])
+const minimumZoom = 1
+const maximumZoom = 30
+svg
+	.attr("transform", "scale(5)")
+export const zoomer = zoom().scaleExtent([minimumZoom, maximumZoom])
 
 svg.call(zoomer.on("zoom", ({ transform }) => {
 	bounds.attr("transform", transform)
@@ -53,32 +60,32 @@ svg.call(zoomer.on("zoom", ({ transform }) => {
 
 ////////////
 
-function centerNode(x, y) {
-	const transform = bounds.node().attributes?.transform?.value
-	const offset = transform
-		? transform.match(/translate\((.+?)\)/)[1].split(",").map(match => +match)
-		: [0, 0]
-	const differential = [
-		offset[0] - x,
-		offset[1] - y,
-	]
-
-	const interpolator = interpolateZoom([offset[0], offset[1], 1], [x, y, 2])
-	const zoomAndPan = (t) => {
-		const view = interpolator(t)
-		const box = bounds.node().getBoundingClientRect()
-		const w = box.width
-		const h = box.height
-		// const k = Math.min(w, h) / view[2]; // scale
-		const k = view[2]; // scale
-		const translate = [
-			w / 2 - view[0] * k,
-			h / 2 - view[1] * k
-		]; // translate
-		console.log(w, h, k, translate)
-		return `translate(${translate[0]},${translate[1]}) scale(${k})`
-		// `translate(${differential[0]},${differential[1]}) scale(2)`
-	}
-
-	bounds.transition().duration(1000).attrTween("transform", () => zoomAndPan)
-}
+// function centerNode(x, y) {
+// 	const transform = bounds.node().attributes?.transform?.value
+// 	const offset = transform
+// 		? transform.match(/translate\((.+?)\)/)[1].split(",").map(match => +match)
+// 		: [0, 0]
+// 	const differential = [
+// 		offset[0] - x,
+// 		offset[1] - y,
+// 	]
+//
+// 	const interpolator = interpolateZoom([offset[0], offset[1], 1], [x, y, 2])
+// 	const zoomAndPan = (t) => {
+// 		const view = interpolator(t)
+// 		const box = bounds.node().getBoundingClientRect()
+// 		const w = box.width
+// 		const h = box.height
+// 		// const k = Math.min(w, h) / view[2]; // scale
+// 		const k = view[2]; // scale
+// 		const translate = [
+// 			w / 2 - view[0] * k,
+// 			h / 2 - view[1] * k
+// 		]; // translate
+// 		console.log(w, h, k, translate)
+// 		return `translate(${translate[0]},${translate[1]}) scale(${k})`
+// 		// `translate(${differential[0]},${differential[1]}) scale(2)`
+// 	}
+//
+// 	bounds.transition().duration(1000).attrTween("transform", () => zoomAndPan)
+// }
